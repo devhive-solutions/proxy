2b04d125a4e659403947fb968ac6940d2cee4701
Nguyễn Ngọc Chinh <47266403+NguyenNgocChinh@users.noreply.github.com>
2023-12-06 01:04:15 +0700

    Update ipv6-with-port-same-username-password.sh

--- 3proxy/ipv6-with-port-same-username-password.sh
+++ 3proxy/ipv6-with-port-same-username-password.sh
@@ -94,8 +94,8 @@ yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "working folder = ~"
-WORKDIR="~"
+echo "working folder = ~/proxy"
+WORKDIR="~/proxy"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
